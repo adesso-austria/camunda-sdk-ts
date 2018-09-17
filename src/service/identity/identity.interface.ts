5db7318e8092864634026127988c3ff15eeb5d0b
Michael Vlasaty <mvlasaty23@gmail.com>
2018-09-17 21:25:22 +0200

    added base profiles for display

--- src/service/identity/identity.interface.ts
+++ src/service/identity/identity.interface.ts
@@ -1,5 +1,18 @@
-
 export interface VerificationResult {
   authenticatedUser: string;
   isAuthenticated: boolean;
 }
+
+export const unassignedUser = {
+  id: 'Unassigned',
+  displayName: 'Unassigned',
+  firstName: 'Unassigned',
+  lastName: undefined,
+};
+
+export const undefinedUser = {
+  displayName: '',
+  id: '',
+  firstName: undefined,
+  lastName: undefined,
+};
